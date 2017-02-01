d49fc7413e9885413ee243e6ed6afd884fc836eb
Ishtiaq Maqsood <java.passion.brains@gmail.com>
2017-02-01 10:41:04 -0600

    -display error message for incorrect formatted future date

--- src/main/java/com/oci/validators/LotteryValidator.java
+++ src/main/java/com/oci/validators/LotteryValidator.java
@@ -29,7 +29,7 @@ public class LotteryValidator implements Validator {
 
         if (!lottery.getPasswordText().equals(lotteryPassword)) {
             errors.rejectValue("passwordText", "PasswordsDontMatch.lotteryForm.passwordText", "Provided password is incorrect");
-        }else if (lottery.getDrawingTime().before(new Date())){
+        } else if (lottery.getDrawingTime() == null || lottery.getDrawingTime().before(new Date())) {
             errors.rejectValue("drawingTime", "PastDrawTimeDate.lotteryForm.drawingTime", "Please provide future draw date in format MM/dd/yyyy hh:mm AM/PM");
         }
     }
