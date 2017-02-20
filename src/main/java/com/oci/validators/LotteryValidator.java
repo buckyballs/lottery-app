9dc9743a94c40e3639a86df6b7c8637c4aa69304
Ishtiaq Maqsood <ishtiaq.maqsood.it@gmail.com>
2017-02-20 17:33:09 -0600

    -Updated spring boot version to 1.5.1 -Removed error controller and relevant code -included validations for participant -enable remote access for h2 console

--- src/main/java/com/oci/validators/LotteryValidator.java
+++ src/main/java/com/oci/validators/LotteryValidator.java
@@ -32,10 +32,10 @@ public class LotteryValidator implements Validator {
         }
         // separate null and valid date parts as we want to show date errors if any of this not valid, removed if else which was limiting error display scope
         if (lottery.getDrawingTime() == null) {
-            errors.rejectValue("minutesToDraw", "NullDrawTimeDate.lotteryForm.drawingTime", "Please Enter valid Minutes To Draw In Positive Number");
+            errors.rejectValue("minutesToDraw", "NullDrawTimeDate.lotteryForm.drawingTime", "Please Enter Valid Minutes To Draw In Positive Number");
         }
         if (lottery.getDrawingTime() != null && lottery.getDrawingTime().before(new Date())) {
-            errors.rejectValue("minutesToDraw", "PastDrawTimeDate.lotteryForm.drawingTime", "Please Enter valid Minutes To Draw In Positive Number");
+            errors.rejectValue("minutesToDraw", "PastDrawTimeDate.lotteryForm.drawingTime", "Please Enter Valid Minutes To Draw In Positive Number");
         }
     }
 }
