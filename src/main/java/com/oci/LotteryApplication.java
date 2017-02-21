2bb7421798a9a025ffcc311742f0927496036baa
Ishtiaq Maqsood <ishtiaq.maqsood.it@gmail.com>
2017-02-21 13:06:55 -0600

    -change spring boot version to 1.3.0.RELEASE from .5.1.RELEASE as deployment to gcloud were giving when app is running  java.lang.NullPointerException: null at org.apache.catalina.authenticator.AuthenticatorBase.getJaspicProvider(AuthenticatorBase.java:1192) ~[tomcat-embed-core-8.5.11.jar!/:8.5.11]

--- src/main/java/com/oci/LotteryApplication.java
+++ src/main/java/com/oci/LotteryApplication.java
@@ -1,19 +1,13 @@
 package com.oci;
 
-import org.apache.catalina.authenticator.jaspic.AuthConfigFactoryImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
-import javax.security.auth.message.config.AuthConfigFactory;
 
 @SpringBootApplication
 public class LotteryApplication {
 
     public static void main(String[] args) {
-        if (AuthConfigFactory.getFactory() == null) {
-            AuthConfigFactory.setFactory(new AuthConfigFactoryImpl());
-        }
         ApplicationContext ctx = SpringApplication.run(LotteryApplication.class, args);
         System.out.println(ctx.getBeanDefinitionCount());
         int i = 1;
